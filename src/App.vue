c4e6cf57ef9c0f9983461e92b74bfc8b378925fa
3crazyspecial <1450815542@qq.com>
2022-04-16 01:06:07 +0800

    feat: add tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <style lang="scss">
