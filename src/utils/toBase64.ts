cd9ff78dca233576621d6114a05ba4c875d369c2
3crazyspecial <1450815542@qq.com>
2022-04-17 00:13:42 +0800

    feat: image view

---
+++ src/utils/toBase64.ts
@@ -0,0 +1,10 @@
+function toBase64(file: Blob): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => resolve(fileReader.result as string);
+    fileReader.onerror = error => reject(error);
+  });
+}
+
+export default toBase64;
